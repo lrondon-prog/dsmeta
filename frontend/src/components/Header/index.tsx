d817b32157e7f379e3de07461e3bc3d5a132c0e9
lrondon-prog <feliperondon123@hotmail.com>
2022-07-13 18:02:46 -0300

    Security config

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -7,7 +7,7 @@ function Header() {
         <header>
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="DSMeta" />
-                <h1>DSMeta</h1>
+                <h1>LR Vendas</h1>
                 <p>
                     Desenvolvido por
                     <a href="https://www.instagram.com/_luisrondon">@_luisrondon</a>
